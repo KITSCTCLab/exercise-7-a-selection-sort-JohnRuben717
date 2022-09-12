4314bf8b4cbbc1415c52b1c643b7b18f8d327522
P. John Ruben Raj <100009511+JohnRuben717@users.noreply.github.com>
2022-09-12 14:35:32 +0530

    1st

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 from typing import List
 
 def selectionSort(array, size) -> List[int]:
+      for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j] > nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
   # Write your code here
 
 # Do not change the following code
@@ -12,3 +18,5 @@ for item in input_data.split(', '):
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
 print(selectionSort(data, len(data)))
+
+
