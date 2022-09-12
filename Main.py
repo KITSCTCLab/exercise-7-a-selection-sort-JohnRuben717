73d3ea2cee89eb387587bb3b2a6ecedafd53af38
P. John Ruben Raj <100009511+JohnRuben717@users.noreply.github.com>
2022-09-12 14:45:39 +0530

    3rd

--- Main.py
+++ Main.py
@@ -1,18 +1,18 @@
 from typing import List
 
 def selectionSort(array, size) -> List[int]:
-for i in range(len(data)):
+     for i in range(len(data)):
      
     # Find the minimum element in remaining
     # unsorted array
-    min_idx = i
-    for j in range(i+1, len(data)):
-        if data[min_idx] > data[j]:
-            min_idx = j
+     min_idx = i
+     for j in range(i+1, len(data)):
+          if data[min_idx] > data[j]:
+               min_idx = j
              
     # Swap the found minimum element with
     # the first element       
-    data[i], data[min_idx] = data[min_idx], data[i]
+     data[i], data[min_idx] = data[min_idx], data[i]
   # Write your code here
 
 # Do not change the following code
