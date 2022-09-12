363066691075542d4bfc1484b00ed5ca5f176e0d
P. John Ruben Raj <100009511+JohnRuben717@users.noreply.github.com>
2022-09-12 15:23:21 +0530

    5th

--- Main.py
+++ Main.py
@@ -1,13 +1,24 @@
 from typing import List
 
 def selectionSort(array, size) -> List[int]:
-
+     size = len(data)  
+      
+    for i in range(length-1):  
+        minIndex = i  
+          
+        for j in range(i+1, size):  
+            if data[j]<data[minIndex]:  
+                minIndex = j  
+                  
+        data[i], data[minIndex] = data[minIndex], data[i]  
+          
+          
+    return data
   # Write your code here
 
 # Do not change the following code
 input_data = input()
 data = []
-print(input_data)
 for item in input_data.split(', '):
   if item.isnumeric():
     data.append(int(item))
